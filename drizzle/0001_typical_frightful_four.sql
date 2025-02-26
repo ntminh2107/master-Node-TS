f63721ee07b59d73b76cac66f63a0b9476c1ded3
ntminh2107 <ntminh2107@gmail.com>
2025-02-26 09:50:48 +0700

    updated

--- drizzle/0001_typical_frightful_four.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "role" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"role_name" varchar(255) NOT NULL
-);
---> statement-breakpoint
-ALTER TABLE "user" DROP CONSTRAINT "user_role_id_user_id_fk";
---> statement-breakpoint
-ALTER TABLE "user" ADD CONSTRAINT "user_role_id_role_id_fk" FOREIGN KEY ("role_id") REFERENCES "public"."role"("id") ON DELETE no action ON UPDATE no action;
--- drizzle/0002_needy_centennial.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "user_info" ADD COLUMN "email" varchar(255) NOT NULL;
